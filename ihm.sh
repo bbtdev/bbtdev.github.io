d250a9d6007247ce76bf3c9edf50f0f23759d9ad
bbtdev <bogdanbuligadev@gmail.com>
2021-03-15 11:33:14 +0200

    home mananger install

--- ihm.sh
+++ ihm.sh
@@ -5,6 +5,6 @@ TEMP_DIR_PATH="~/Temp"
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-$VERSION.tar.gz home-manager
 nix-channel --update
 mkdir $TEMP_DIR_PATH -p
-echo $COMMAND >> $TEMP_DIR_PATH/$FILE
+echo $COMMAND >> "$TEMP_DIR_PATH/$FILE"
 echo "Logout and use (also saved in Temp/$FILE): $COMMAND"
 
