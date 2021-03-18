381502a6bce4b28a3881443f09202f1424412170
bbtdev <bogdanbuligadev@gmail.com>
2021-03-18 10:35:58 +0200

    updated gscc.sh

--- gscc.sh
+++ gscc.sh
@@ -1,9 +1,6 @@
 mkdir -p ~/.ssh/
-ssh-keygen -f ~/.ssh/ids_rsa_bbtdev
-cat ~/.ssh/ids_rsa_bbtdev.pub | xclip -selection c
+ssh-keygen -f ~/.ssh/ids_rsa
 echo "Key copied to clipboard!"
-ssh-add ~/.ssh/ids_rsa_bbtdev
+cat ~/.ssh/ids_rsa.pub | xclip -selection c
 echo "Key should appear here:"
 ssh-add -l
-echo "If not 'ssh-add -D && ssh-add ~/.ssh/ids_rsa_bbtdev'"
-echo "'ssh-add -l'"
