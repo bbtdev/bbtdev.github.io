2227fb6669695bd645852c495d9406a604dc7e55
bbtdev <bogdanbuligadev@gmail.com>
2021-03-18 10:16:38 +0200

    updated gscc.sh

--- gscc.sh
+++ gscc.sh
@@ -1,3 +1,7 @@
 mkdir -p ~/.ssh/
 ssh-keygen -f ~/.ssh/ids_rsa_bbtdev
+ssh-add ~/.ssh/id_rsa_bbtdev
+ssh-add -D
+ssh-add -l
 cat ~/.ssh/ids_rsa_bbtdev.pub | xclip -selection c
+echo "Key copied to clipboard!"
